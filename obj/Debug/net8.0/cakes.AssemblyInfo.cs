bd4d50369fc3d3193f9e74a6140d7251b8305bde
Drew Wilson <jankyy93@gmail.com>
2024-01-17 08:41:46 -0500

    Front-end initial commit

--- obj/Debug/net8.0/cakes.AssemblyInfo.cs
+++ obj/Debug/net8.0/cakes.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("cakes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb7898079faf145987fdcec5210990962fc96792")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f411f1d68dfbeb3ea39b8c99d6442ed26e7a923")]
 [assembly: System.Reflection.AssemblyProductAttribute("cakes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("cakes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
